5446120cce066c7661c78a61857d39c02dc1401a
Jose Ferreira <josebazjunior09@gmail.com>
2024-01-01 11:55:44 -0300

    [feat]: add fastapi initialization

---
+++ backend/api/app.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="API - Open source project observatory",
+    version="0.1.0",
+    description="FastAPI/Python - Open source project observatory"
+)
