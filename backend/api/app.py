b1009d0761016cce9c523963117e1610a3d9911d
Jose Ferreira <josebazjunior09@gmail.com>
2024-01-01 18:33:32 -0300

    [feat]: add health_check endpoint

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from api.controllers import main_router
 
 app = FastAPI(
     title="API - Open source project observatory",
     version="0.1.0",
     description="FastAPI/Python - Open source project observatory"
 )
+app.include_router(main_router)
