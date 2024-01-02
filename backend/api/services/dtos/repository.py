7830c8f71c301bcaf28d09bd99cb2cb42398ce5b
Jose Ferreira <josebazjunior09@gmail.com>
2024-01-02 12:43:11 -0300

    [feat]: add repository graphics response

--- backend/api/services/dtos/repository.py
+++ backend/api/services/dtos/repository.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import List, Optional
+from .repository_graphics import IssueGraphicResponse, PullRequestGraphicResponse
 
 
 class RepositoryResponse(BaseModel):
@@ -19,10 +20,12 @@ class RepositoryResponse(BaseModel):
     issues_amount: Optional[int]
     prs_amount: Optional[int]
     commits_amount: Optional[int]
+    issues_graphic: Optional[IssueGraphicResponse]
+    prs_graphic: Optional[PullRequestGraphicResponse]
 
     @classmethod
-    def from_repository_response(cls, owner: str, name: str, github_response) -> "RepositoryResponse":
-        repository_data = github_response.get("data", None).get("repository", None)
+    def from_repository_response(cls, owner: str, name: str, repo_response) -> "RepositoryResponse":
+        repository_data = repo_response.get("data", None).get("repository", None)
 
         return cls(
             owner=owner,
@@ -37,12 +40,21 @@ class RepositoryResponse(BaseModel):
             stars_amount=repository_data.get("stargazers", None).get("totalCount", None),
             readme_url=None,
             contribution_url=None,
-            contributors_amount = repository_data.get("mentionableUsers", None).get("totalCount", None),
+            contributors_amount=repository_data.get("mentionableUsers", None).get("totalCount", None),
             forks_amount=repository_data.get("forks", None).get("totalCount", None),
             branches_amount=repository_data.get("refs", None).get("totalCount", None),
             issues_amount=repository_data.get("issues", None).get("totalCount", None),
             prs_amount=repository_data.get("pullRequests", None).get("totalCount", None),
             commits_amount=repository_data.get("defaultBranchRef", None).get("target", None).get("totalCommits",
                                                                                                  None).get("totalCount",
-                                                                                                           None)
+                                                                                                           None),
+            issues_graphic=IssueGraphicResponse(
+                open_issues=repository_data.get("openIssues", None).get("totalCount", None),
+                closed_issues=repository_data.get("closedIssues", None).get("totalCount", None)
+            ),
+            prs_graphic=PullRequestGraphicResponse(
+                open_pull_requests=repository_data.get("openPullRequests", None).get("totalCount", None),
+                closed_pull_requests=repository_data.get("closedPullRequests", None).get("totalCount", None),
+                merged_pull_requests=repository_data.get("mergedPullRequests", None).get("totalCount", None)
+            )
         )
---
+++ backend/api/services/dtos/repository_graphics.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class IssueGraphicResponse(BaseModel):
+    open_issues: Optional[int]
+    closed_issues: Optional[int]
+
+
+class PullRequestGraphicResponse(BaseModel):
+    open_pull_requests: Optional[int]
+    closed_pull_requests: Optional[int]
+    merged_pull_requests: Optional[int]
