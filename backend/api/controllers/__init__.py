c04adc40c32637d98f7c2753dd3afadd3fe66b44
Jose Ferreira <josebazjunior09@gmail.com>
2024-01-02 01:47:01 -0300

    [feat]: add repository controller logic

--- backend/api/controllers/__init__.py
+++ backend/api/controllers/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .health_check import router as health_check_router
+from .repository import router as repo_router
 
 main_router = APIRouter()
 
 main_router.include_router(health_check_router, prefix="/health_check", tags=['Health check'])
+main_router.include_router(repo_router, prefix="/repositories", tags=['Repositories'])
---
+++ backend/api/controllers/repository.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Path
+from api.services.dtos.repository import RepositoryResponse
+from api.services.repository import RepositoryService
+
+router = APIRouter()
+
+
+@router.get("/{owner}/{repo_name}", response_model=RepositoryResponse)
+def get_repository(owner: str = Path(..., title="Owner of the repository"),
+                   repo_name: str = Path(..., title="Name of the repository")):
+    return RepositoryService.get_repository(owner=owner, repo_name=repo_name)
